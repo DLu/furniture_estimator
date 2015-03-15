9afcfafc51528097a11e41f060c902ee42d83cb2
David V. Lu!! <davidvlu@gmail.com>
2015-03-15 13:45:33 -0500

    Kalman

--- src/wheelchair.py
+++ src/wheelchair.py
@@ -4,6 +4,7 @@ import sys
 from pylab import *
 from furniture_estimator import *
 from furniture_estimator.wheelchair import *
+from furniture_estimator.kalman import *
 
 def plotp(pts, symbol='.', color='blue'):
     plot([p[0] for p in pts], [p[1] for p in pts], symbol, color=color)
@@ -11,13 +12,23 @@ def plotp(pts, symbol='.', color='blue'):
 bag = rosbag.Bag(sys.argv[1])
 pts = []
 
+k1 = Kalman()
+k2 = Kalman()
+
 for a,b,c in bag.read_messages():
     if a!='/base_scan':
         continue
-    lpt, rpt = filter_scan(b)    
+    lpt, rpt = filter_scan(b)
+    k1.update(lpt)
+    k2.update(rpt)
     pts.append(lpt)
     pts.append(rpt)
 
-plotp(pts, color='green', symbol='o')
+    la = k1.values()
+    ra = k2.values()
+
+    #plotp(pts, color='green', symbol='.')
+    plotp( [la, ra], color='red')
+    #show()
 show()
         
