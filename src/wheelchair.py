fb8279b14ccaf9b871e4daf7e11759f25812ce07
David V. Lu!! <davidvlu@gmail.com>
2015-03-16 20:16:03 -0500

    Wheelchair with frame

--- src/wheelchair.py
+++ src/wheelchair.py
@@ -3,6 +3,7 @@ import rospy
 from furniture_estimator import *
 from furniture_estimator.wheelchair import *
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import PoseStamped
 import tf
 
 class WheelchairNode:
@@ -10,8 +11,10 @@ class WheelchairNode:
         rospy.init_node('wheelchair_monitor')
         self.wf = WheelchairFilter()
         self.br = tf.TransformBroadcaster()
+        self.tf = tf.TransformListener()
         self.sub = rospy.Subscriber('/base_scan', LaserScan, self.laser_cb)
         self.frame = None
+        self.broadcast_frame = '/odom_combined'
 
     def laser_cb(self, msg):
         self.frame = msg.header.frame_id
@@ -20,14 +23,33 @@ class WheelchairNode:
     def spin(self):
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
+            r.sleep()
             pose = self.wf.get_pose()
-            if pose:
-                self.br.sendTransform((pose[0], pose[1], 0),
-                             tf.transformations.quaternion_from_euler(0, 0, pose[2]),
-                             rospy.Time.now(),
-                             '/wheelchair',
-                             self.frame)
-            r.sleep()                 
+            if not pose:
+                continue  
+                
+            ps = PoseStamped()
+            ps.header.frame_id = self.frame
+            ps.pose.position.x = pose[0]
+            ps.pose.position.y = pose[1]
+            q = tf.transformations.quaternion_from_euler(0, 0, pose[2])
+            ps.pose.orientation.x = q[0]
+            ps.pose.orientation.y = q[1]
+            ps.pose.orientation.z = q[2]
+            ps.pose.orientation.w = q[3]
+
+            try:
+                np = self.tf.transformPose(self.broadcast_frame, ps)
+            except:
+                continue
+
+            pos = np.pose.position
+            q = np.pose.orientation    
+            self.br.sendTransform((pos.x, pos.y, pos.z),
+                         (q.x, q.y, q.z, q.w),
+                         rospy.Time.now(),
+                         '/wheelchair',
+                         np.header.frame_id)
 
 if __name__ == '__main__':
     n = WheelchairNode()
